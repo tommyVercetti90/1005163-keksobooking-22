df6bdf072a8c16320f8360f8a8871237105cbbb8
Ilnar <ilnarshan@gmail.com>
2021-03-05 09:05:21 +0400

    5.отрисуй меня полностью (часть 2).Исправил замечания наставника

--- js/form.js
+++ js/form.js
@@ -1,28 +1,28 @@
-const offerForm = document.querySelector('.ad-form');
-const propertyType = offerForm.querySelector('#type');
-const offerPrice = offerForm.querySelector('#price');
-const checkInTime = offerForm.querySelector('#timein');
-const checkOutTime = offerForm.querySelector('#timeout');
+const offerForm = document.querySelector(".ad-form");
+const propertyType = offerForm.querySelector("#type");
+const offerPrice = offerForm.querySelector("#price");
+const checkInTime = offerForm.querySelector("#timein");
+const checkOutTime = offerForm.querySelector("#timeout");
 
 // Функция замены цены в placeholder
 function changePrice() {
   function changeAttributePrice(price) {
     offerPrice.placeholder = price.toString();
-    offerPrice.setAttribute('placeholder', price);
+    offerPrice.setAttribute("min", price);
   }
 
-  propertyType.addEventListener('change', function (evt) {
+  propertyType.addEventListener("change", function (evt) {
     switch (evt.target.value) {
-      case 'bungalow':
+      case "bungalow":
         changeAttributePrice(0);
         break;
-      case 'flat':
+      case "flat":
         changeAttributePrice(1000);
         break;
-      case 'house':
+      case "house":
         changeAttributePrice(5000);
         break;
-      case 'palace':
+      case "palace":
         changeAttributePrice(10000);
         break;
       default:
@@ -34,17 +34,17 @@ function changePrice() {
 
 // Функция валидации цены
 function validatePrice() {
-  offerPrice.addEventListener('invalid', validationPrice);
+  offerPrice.addEventListener("invalid", validationPrice);
 
   function validationPrice() {
     if (propertyType.options[1] && offerPrice.value < 1000) {
-      offerPrice.setCustomValidity('Цена должна быть не менее 1000 руб.');
+      offerPrice.setCustomValidity("Цена должна быть не менее 1000 руб.");
     } else if (propertyType.options[2] && offerPrice.value < 5000) {
-      offerPrice.setCustomValidity('Цена должна быть не менее 5000 руб.');
+      offerPrice.setCustomValidity("Цена должна быть не менее 5000 руб.");
     } else if (propertyType.options[3] && offerPrice.value < 10000) {
-      offerPrice.setCustomValidity('Цена должна быть не менее 10000 руб.');
+      offerPrice.setCustomValidity("Цена должна быть не менее 10000 руб.");
     } else {
-      offerPrice.setCustomValidity('');
+      offerPrice.setCustomValidity("");
     }
   }
 }
@@ -58,8 +58,8 @@ let syncCheckOutTime = function (evt) {
 };
 
 function syncCheckTime() {
-  checkInTime.addEventListener('change', syncCheckInTime);
-  checkOutTime.addEventListener('change', syncCheckOutTime);
+  checkInTime.addEventListener("change", syncCheckInTime);
+  checkOutTime.addEventListener("change", syncCheckOutTime);
 }
 
 export { changePrice, validatePrice, syncCheckTime };
